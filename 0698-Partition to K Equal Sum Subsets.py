2d0f244f62c1aa56dba37bae54701fcb9dab263d
SoundsOfLife <SoundsOfLife@users.noreply.github.com>
2018-02-03 23:10:11 +0800

    ~

--- 0698-Partition to K Equal Sum Subsets.py
+++ 0698-Partition to K Equal Sum Subsets.py
@@ -1,17 +1,33 @@
-class Solution:
+###DFS
+
+class Solution(object):
     def canPartitionKSubsets(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
-        """
-        sums = sum(nums)
-        nums = sorted(nums)
-        print(nums)
-        if sums % k != 0:
+        if k == 1: return True
+
+        self.n = len(nums)
+        if k > self.n: return False
+
+        total = sum(nums)
+        if total % k: return False
+
+        self.target = total / k
+        visit = [0] * self.n
+
+        nums.sort(reverse=True)
+
+        def dfs(k, ind, sum, cnt):
+            if k == 1: return True
+            if sum == self.target and cnt > 0:
+                return dfs(k - 1, 0, 0, 0)
+            for i in range(ind, self.n):
+                if not visit[i] and sum + nums[i] <= self.target:
+                    visit[i] = 1
+                    if dfs(k, i + 1, sum + nums[i], cnt + 1):
+                        return True
+                    visit[i] = 0
             return False
-        else:
-            s = sums % k
+
+        return dfs(k, 0, 0, 0)
 
 
 if __name__ == '__main__':
